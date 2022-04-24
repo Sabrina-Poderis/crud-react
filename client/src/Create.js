49c88b165eae30410fd02564e47ec28f007b0dad
Sabrina Poderis <sabrina.poderis@gmail.com>
2022-04-24 19:54:00 -0300

    route getGames and List implementation

--- client/src/Create.js
+++ client/src/Create.js
@@ -12,7 +12,7 @@ export default function Create(){
     }
 
     const handleClickButton = () => {
-        Axios.post("http://127.0.0.1:3001/register", {
+        Axios.post("http://127.0.0.1:3001/store", {
             name: values.name,
             cost: values.cost,
             category: values.category,
@@ -32,61 +32,59 @@ export default function Create(){
                     </div>
 
                     <div class="w-full px-6 py-4 bg-white rounded shadow-md ring-1 ring-gray-900/10">
-                        {/* <form method="POST" action=""> */}
-                            <div>
-                                <label class="block text-sm font-bold text-gray-700" for="name">
-                                    Nome
-                                </label>
+                        <div>
+                            <label class="block text-sm font-bold text-gray-700" for="name">
+                                Nome
+                            </label>
 
-                                <input
-                                    class="block w-full mt-1 border-gray-300 rounded-md shadow-sm placeholder:text-gray-400 placeholder:text-right focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                    type="text"
-                                    name="name"
-                                    placeholder="Nome"
-                                    onChange={handleChangeValues}/>
-                            </div>
+                            <input
+                                class="block w-full mt-1 border-gray-300 rounded-md shadow-sm placeholder:text-gray-400 placeholder:text-right focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                type="text"
+                                name="name"
+                                placeholder="Nome"
+                                onChange={handleChangeValues}/>
+                        </div>
 
-                            <div class="mt-4">
-                                <label class="block text-sm font-bold text-gray-700" for="cost">
-                                    Preço
-                                </label>
-                                <input
-                                    class="block w-full mt-1 border-gray-300 rounded-md shadow-sm placeholder:text-gray-400 placeholder:text-right focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                    type="text"
-                                    name="cost"
-                                    placeholder="R$ 0,00"
-                                    onChange={handleChangeValues}
-                                />
-                            </div>
+                        <div class="mt-4">
+                            <label class="block text-sm font-bold text-gray-700" for="cost">
+                                Preço
+                            </label>
+                            <input
+                                class="block w-full mt-1 border-gray-300 rounded-md shadow-sm placeholder:text-gray-400 placeholder:text-right focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                type="text"
+                                name="cost"
+                                placeholder="R$ 0,00"
+                                onChange={handleChangeValues}
+                            />
+                        </div>
 
-                            <div class="mt-4">
-                                <label class="block text-sm font-bold text-gray-700" for="category">
-                                    Categoria
-                                </label>
-                                <input
-                                    class="block w-full mt-1 border-gray-300 rounded-md shadow-sm placeholder:text-gray-400 placeholder:text-right focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                    type="text"
-                                    name="category"
-                                    placeholder="Categoria"
-                                    onChange={handleChangeValues}
-                                />
-                            </div>
+                        <div class="mt-4">
+                            <label class="block text-sm font-bold text-gray-700" for="category">
+                                Categoria
+                            </label>
+                            <input
+                                class="block w-full mt-1 border-gray-300 rounded-md shadow-sm placeholder:text-gray-400 placeholder:text-right focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                type="text"
+                                name="category"
+                                placeholder="Categoria"
+                                onChange={handleChangeValues}
+                            />
+                        </div>
 
-                            <div class="flex items-center justify-start mt-4 gap-x-2">
-                                <button 
-                                    onClick={() => handleClickButton()}
-                                    class="px-6 py-2 text-sm font-semibold rounded-md shadow-md text-sky-100 bg-sky-500 hover:bg-sky-700 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300"
-                                >
-                                    Salvar
-                                </button>
-                                <button 
-                                    type="submit"
-                                    class="px-6 py-2 text-sm font-semibold text-gray-100 bg-gray-400 rounded-md shadow-md hover:bg-gray-600 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300"
-                                >
-                                    Cancelar
-                                </button>
-                            </div>
-                        {/* </form> */}
+                        <div class="flex items-center justify-start mt-4 gap-x-2">
+                            <button 
+                                onClick={() => handleClickButton()}
+                                class="px-6 py-2 text-sm font-semibold rounded-md shadow-md text-sky-100 bg-sky-500 hover:bg-sky-700 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300"
+                            >
+                                Salvar
+                            </button>
+                            <a 
+                                href="/"
+                                class="px-6 py-2 text-sm font-semibold text-gray-100 bg-gray-400 rounded-md shadow-md hover:bg-gray-600 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300"
+                            >
+                                Cancelar
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
--- client/src/List.js
+++ client/src/List.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios"
 
 export default function List(){
+    const [listGames, setListGames] = useState()
+
+    useEffect(() => {
+        Axios.get("http://127.0.0.1:3001/getGames").then((response) => {
+            setListGames(response.data)
+        })
+    }, [])
+    
     return (
         <div class="container max-w-7xl mx-auto mt-8">
             <div class="mb-4">
@@ -36,144 +45,54 @@ export default function List(){
                             </thead>
 
                             <tbody class="bg-white">
-                                <tr>
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                        <div class="flex items-center">
-                                            1
-                                        </div>
-                                    </td>
+                                {typeof listGames !== 'undefined' && listGames.map((value) => {
+                                    return (
+                                        <tr key={value.id}>
+                                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                                <div class="flex items-center">
+                                                    {value.id}
+                                                </div>
+                                            </td>
 
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                        <div class="text-sm leading-5 text-gray-900">
-                                            Jogo 1
-                                        </div>
-                                    </td>
+                                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                                <div class="text-sm leading-5 text-gray-900">
+                                                    {value.name}
+                                                </div>
+                                            </td>
 
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                        <p>R$1,00</p>
-                                    </td>
+                                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                                <p>R${value.cost}</p>
+                                            </td>
 
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                        <p>Categoria 1</p>
-                                    </td>
+                                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                                <p>{value.category}</p>
+                                            </td>
 
-                                    <td class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
-                                        <span>12/12/22</span>
-                                    </td>
+                                            <td class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
+                                                <span>{value.created_at}</span>
+                                            </td>
 
-                                    <td class="text-sm font-medium leading-5 text-center whitespace-no-wrap border-b border-gray-200 ">
-                                        <a href="#" class="text-indigo-600 hover:text-indigo-900">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
-                                                stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                                            </svg>
-                                        </a>
-                                    </td>
-                                    <td class="text-sm font-medium leading-5 whitespace-no-wrap border-b border-gray-200 ">
-                                        <a href="#">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-red-600 hover:text-red-800"
-                                                fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                            </svg>
-                                        </a>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                        <div class="flex items-center">
-                                            2
-                                        </div>
-
-                                    </td>
-
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                        <div class="text-sm leading-5 text-gray-900">
-                                            Jogo 2
-                                        </div>
-                                    </td>
-
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                        <p>R$2,00</p>
-                                    </td>
-
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                        <p>Categoria 2</p>
-                                    </td>
-
-                                    <td class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
-                                        <span>12/12/22</span>
-                                    </td>
-
-                                    <td class="text-sm font-medium leading-5 text-center whitespace-no-wrap border-b border-gray-200 ">
-                                        <a href="#" class="text-indigo-600 hover:text-indigo-900">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
-                                                stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                                            </svg>
-                                        </a>
-                                    </td>
-
-                                    <td class="text-sm font-medium leading-5 whitespace-no-wrap border-b border-gray-200 ">
-                                        <a href="#">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-red-600 hover:text-red-800"
-                                                fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                            </svg>
-                                        </a>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                        <div class="flex items-center">
-                                        3
-                                        </div>
-
-                                    </td>
-
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                        <div class="text-sm leading-5 text-gray-900">
-                                            Jogo 3
-                                        </div>
-                                    </td>
-
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                        <p>R$3,00</p>
-                                    </td>
-
-                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                        <p>Categoria 3</p>
-                                    </td>
-
-                                    <td class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
-                                        <span>12/12/22</span>
-                                    </td>
-
-                                    <td class="text-sm font-medium leading-5 text-center whitespace-no-wrap border-b border-gray-200 ">
-                                        <a href="#" class="text-indigo-600 hover:text-indigo-900">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
-                                                stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                                            </svg>
-                                        </a>
-                                    </td>
-                               
-                                    <td class="text-sm font-medium leading-5 whitespace-no-wrap border-b border-gray-200 ">
-                                        <a href="#">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-red-600 hover:text-red-800"
-                                            fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                            </svg>
-                                        </a>
-                                    </td>
-                                </tr>
+                                            <td class="text-sm font-medium leading-5 text-center whitespace-no-wrap border-b border-gray-200 ">
+                                                <a href="#" class="text-indigo-600 hover:text-indigo-900">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
+                                                        stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                                    </svg>
+                                                </a>
+                                            </td>
+                                            <td class="text-sm font-medium leading-5 whitespace-no-wrap border-b border-gray-200 ">
+                                                <a href="#">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-red-600 hover:text-red-800"
+                                                        fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                    </svg>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
                             </tbody>
                         </table>
                     </div>
